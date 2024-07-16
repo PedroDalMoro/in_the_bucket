5c23b2c39992c63e1419f577e18d1c21764fb1db
lassira <larissa.pasinato@gmail.com>
2024-07-15 21:40:19 -0300

    remove x_pos, y_pos

--- balde.cpp
+++ balde.cpp
@@ -1,10 +1,8 @@
 #include "balde.hpp"
 
 
-Balde::Balde(int pos_x, int pos_y, int width, int height, Color color)
+Balde::Balde(int width, int height, Color color)
 {
-    _pos_x = pos_x;
-    _pos_y = pos_y;
     _width = width;
     _height = height;
     _color = color;
--- balde.hpp
+++ balde.hpp
@@ -6,14 +6,12 @@
 class Balde
 {
 private:
-    int _pos_x;
-    int _pos_y;
     int _width;
     int _height;
     Color _color;
 
 public:
-    Balde(int pos_x, int pos_y, int width, int height, Color color);
+    Balde(int width, int height, Color color);
     ~Balde();
 
     void draw(int mouse_x, int mouse_y);
