1ad2c76d02f3ada3fae0fddfe6feab6b0fc2d14b
PedroDalMoro <pedrocentenaro22@gmail.com>
2024-08-12 22:10:52 -0300

    add vector, testado e funcionando

--- vector_2.cpp
+++ vector_2.cpp
@@ -1,1 +1,116 @@
 #include "vector_2.hpp"
+
+// funções privadas
+
+void Vec2::_copy(float x, float y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+void Vec2::_scalar(float scalar)
+{
+    this->x *= scalar;
+    this->y *= scalar;
+}
+
+void Vec2::_add(float x, float y)
+{
+    this->x += x;
+    this->y += y;
+}
+
+void Vec2::_subtract(float x, float y)
+{
+    this->x -= x;
+    this->y -= y;
+}
+
+// construtores
+
+Vec2::Vec2(float x, float y)
+{
+    _copy(x, y);
+}
+
+Vec2::~Vec2()
+{
+}
+
+// funções públicas
+
+void Vec2::operator = (Vec2 item)
+{
+    _copy(item.x, item.y);
+}
+
+void Vec2::operator *= (float scalar)
+{
+    _scalar(scalar);
+}
+
+void Vec2::operator += (Vec2 item)
+{
+    _add(item.x, item.y);
+}
+
+void Vec2::operator -= (Vec2 item)
+{
+    _subtract(item.x, item.y);
+}
+
+void Vec2::set(float x, float y)
+{
+    _copy(x, y);
+}
+
+void Vec2::copy(Vec2 other)
+{
+    _copy(other.x, other.y);
+}
+
+void Vec2::scalar(float scalar)
+{
+    _scalar(scalar);
+}
+
+void Vec2::add(float x, float y)
+{
+    _add(x, y);
+}
+
+void Vec2::add(Vec2 other)
+{
+    _add(other.x, other.y);
+}
+
+void Vec2::subtract(float x, float y)
+{
+    _subtract(x, y);
+}
+
+void Vec2::subtract(Vec2 other)
+{
+    _subtract(other.x, other.y);
+}
+
+float Vec2::getLength()
+{
+    return (std::sqrt(std::pow(this->x, 2.0f) + std::pow(this->y, 2.0f)));
+}
+
+Vec2 Vec2::getNormalized()
+{
+    Vec2 ret = { x = this->x, y = this->y };
+    float length = this->getLength();
+
+    ret.x /= length;
+    ret.y /= length;
+
+    return (ret);
+}
+
+float Vec2::dotProduct(Vec2 other)
+{
+    return ((this->x * other.x) + (this->y * other.y));
+}
--- vector_2.hpp
+++ vector_2.hpp
@@ -3,9 +3,20 @@
 
 #include <cmath>
 
+/**
+ * @brief Classe que lida com as operações matemáticas nos vetores (x e y). 
+ * 
+ * TODO: testei todas as operações, mas se algum dia a gente fizer as coisas de unit tests, 
+ * essa classe seria um lugar bem bom pra começar, porque todas as funções tem um resultado 
+ * esperado claro, sem outros efeitos. 
+ */
 class Vec2
 {
 private:
+    void _copy(float x, float y);                    // atribui um vetor ao outro 
+    void _scalar(float scalar);                      // multiplica o vetor por um escalar
+    void _add(float x, float y);                     // soma valores ao vetor 
+    void _subtract(float x, float y);                // diminui valores do vetor
 
 public:
     float x;
@@ -20,184 +31,19 @@ public:
     void operator += (Vec2 item);                   // soma um vetor ao outro 
     void operator -= (Vec2 item);                   // diminui um vetor do outro (primeiro menos o segundo)
 
-    // funções pra matemática de vetores
+    // mesmo que os operadores, mas com funções
+    void set(float x, float y);                     // atribui valores ao vetor
+    void copy(Vec2 other);                          // atribui um vetor ao outro 
+    void scalar(float scalar);                      // multiplica o vetor por um escalar
+    void add(float x, float y);                     // soma valores ao vetor 
+    void add(Vec2 other);                           // soma um vetor ao outro 
+    void subtract(float x, float y);                // diminui valores do vetor
+    void subtract(Vec2 other);                      // diminui um vetor do outro (this - other)
+
+    // outras funções pra matemática de vetores
     float getLength();                              // retorna o tamanho (módulo) do vetor
     Vec2 getNormalized();                           // retorna o vetor normalizado (dividido pelo módulo)
     float dotProduct(Vec2 other);                   // retorna o dot procutd ((vx * ux) + (vy * uy))
-
 };
 
-Vec2::Vec2(float x, float y)
-{
-    this->x = x;
-    this->y = y;
-}
-
-Vec2::~Vec2()
-{
-}
-
-void Vec2::operator = (Vec2 item)
-{
-    this->x = item.x;
-    this->y = item.y;
-}
-
-void Vec2::operator *= (float scalar)
-{
-    this->x *= scalar;
-    this->y *= scalar;
-}
-
-void Vec2::operator += (Vec2 item)
-{
-    this->x += item.x;
-    this->y += item.y;
-}
-
-void Vec2::operator -= (Vec2 item)
-{
-    this->x -= item.x;
-    this->y -= item.y;
-}
-
-float Vec2::getLength()
-{
-    return (std::sqrt(std::pow(this->x, 2.0f) + std::pow(this->y, 2.0f)));
-}
-
-Vec2 Vec2::getNormalized()
-{
-    Vec2 ret = { x = this->x, y = this->y };
-    float length = this->getLength();
-
-    ret.x /= length;
-    ret.y /= length;
-
-    return (ret);
-}
-
-float Vec2::dotProduct(Vec2 other)
-{
-    return ((this->x * other.x) + (this->y * other.y));
-}
-
 #endif  // VECTOR_2_HPP
-
-
-// COISAS PRA TESTAR USANDO O COMPILADOR ONLINE, APAGAR DEPOIS E TENHO AINDA QUE MUDAR AS COISAS PRO CPP
-
-// // Online C++ compiler to run C++ program online
-// #include <iostream>
-// #include <cmath>
-
-// using namespace std;
-
-// class Vec2
-// {
-// private:
-
-// public:
-//     float x;
-//     float y;
-
-//     Vec2(float x = 0.0f, float y = 0.0f);
-//     ~Vec2();
-
-//     // operadores pra lidar direto com os vetores
-//     void operator = (Vec2 item);                    // atribui um vetor ao outro 
-//     void operator *= (float scalar);                // multiplica o vetor por um escalar
-//     void operator += (Vec2 item);                   // soma um vetor ao outro 
-//     void operator -= (Vec2 item);                   // diminui um vetor do outro (primeiro menos o segundo)
-
-//     // funções pra matemática de vetores
-//     float getLength();                              // retorna o tamanho (módulo) do vetor
-//     Vec2 getNormalized();                           // retorna o vetor normalizado (dividido pelo módulo)
-//     float dotProduct(Vec2 other);                   // retorna o dot procutd ((vx * ux) + (vy * uy))
-
-// };
-
-// Vec2::Vec2(float x, float y)
-// {
-//     this->x = x;
-//     this->y = y;
-// }
-
-// Vec2::~Vec2()
-// {
-// }
-
-// void Vec2::operator = (Vec2 item)
-// {
-//     this->x = item.x;
-//     this->y = item.y;
-// }
-
-// void Vec2::operator *= (float scalar)
-// {
-//     this->x *= scalar;
-//     this->y *= scalar;
-// }
-
-// void Vec2::operator += (Vec2 item)
-// {
-//     this->x += item.x;
-//     this->y += item.y;
-// }
-
-// void Vec2::operator -= (Vec2 item)
-// {
-//     this->x -= item.x;
-//     this->y -= item.y;
-// }
-
-// float Vec2::getLength()
-// {
-//     return (std::sqrt(std::pow(this->x, 2.0f) + std::pow(this->y, 2.0f)));
-// }
-
-// Vec2 Vec2::getNormalized()
-// {
-//     Vec2 ret = { x = this->x, y = this->y };
-//     float length = this->getLength();
-
-//     ret.x /= length;
-//     ret.y /= length;
-
-//     return (ret);
-// }
-
-// float Vec2::dotProduct(Vec2 other)
-// {
-//     return ((this->x * other.x) + (this->y * other.y));
-// }
-
-// int main() {
-
-//     Vec2 values(12, 24);
-//     Vec2 other(100, 200);
-    
-//     cout << "valores iniciais" << endl;
-//     cout << values.x << " - " << values.y << endl;
-    
-//     values = other;
-//     cout << "testando atribuição" << endl;
-//     cout << values.x << " - " << values.y << endl;
-    
-//     values *= 1.5;
-//     cout << "testando escalar" << endl;
-//     cout << values.x << " - " << values.y << endl;
-    
-//     values.x = 1.234;
-//     values.y = 3.12;
-//     cout << "testando módulo" << endl;
-//     cout << values.getLength() << endl;
-
-//     values.x = 3;
-//     values.y = 4;
-//     Vec2 temp = values.getNormalized();
-//     cout << "testando normalizado" << endl;
-//     cout << temp.x << " - " << temp.y << endl;
-
-//     return 0;
-// }
