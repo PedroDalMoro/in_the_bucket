c56aff19e9692827ee7e1b1679c5f09f5251a670
PedroDalMoro <pedrocentenaro22@gmail.com>
2024-09-18 17:50:04 -0300

    add colisão entre as bolas, funcionando legal

--- include/bola.hpp
+++ include/bola.hpp
@@ -10,21 +10,22 @@
 class Bola
 {
 private:
-    Vec2 _pos;
-    Vec2 _vel;
-    float _rad;
-    Color _cor;
-
     int _map_x(float pos_x);
     int _map_y(float pos_y);
     int _map_rad(float rad);
 
 public:
+    Vec2 pos;
+    Vec2 vel;
+    float rad;
+    float mass;
+    Color cor;
+    
     Bola(float pos_x_meters, float pos_y_meters, float vel_x_mps, float vel_y_mps, float rad_meters, Color cor);
     Bola();
     ~Bola();
 
-    void init(float pos_x_meters, float pos_y_meters, float vel_x_mps, float vel_y_mps, float rad_meters, Color cor);
+    void init(float pos_x_meters, float pos_y_meters, float vel_x_mps, float vel_y_mps, float rad_meters, Color color);
     void update();
     void draw();
 };
