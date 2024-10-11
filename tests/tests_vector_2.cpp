21e74dc36ca588c18f21f57dc8b7b4fd183411b5
PedroDalMoro <pedrocentenaro22@gmail.com>
2024-10-10 23:30:23 -0300

    sera se agora vai?

--- tests/tests_vector_2.cpp
+++ tests/tests_vector_2.cpp
@@ -1,16 +1,16 @@
 #include "catch2/catch_amalgamated.hpp"
-// #include "vector_2.hpp"
+#include "vector_2.hpp"
 
-// TEST_CASE("Testing set vector values")
-// {
-//     Vec2 sut(0, 0); 
-
-//     sut.set(5, 5);
-//     REQUIRE(sut.x == 5.0f);
-//     REQUIRE(sut.y == 5.0f);
-// }
-
-TEST_CASE("Another test case")
+TEST_CASE("Testing set vector values")
 {
-    REQUIRE(1 == 1);
+    Vec2 sut(0, 0); 
+
+    sut.set(5, 5);
+    REQUIRE(sut.x == 5.0f);
+    REQUIRE(sut.y == 5.0f);
 }
+
+// TEST_CASE("Another test case")
+// {
+//     REQUIRE(1 == 1);
+// }
