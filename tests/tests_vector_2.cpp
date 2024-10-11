1457e454588154bdd93aad1ea9f352ef77b94f7f
PedroDalMoro <pedrocentenaro22@gmail.com>
2024-10-10 23:19:06 -0300

    fazendo um teste simples sem nada

--- tests/tests_vector_2.cpp
+++ tests/tests_vector_2.cpp
@@ -1,21 +1,16 @@
 #include "catch2/catch_amalgamated.hpp"
-#include "vector_2.hpp"
+// #include "vector_2.hpp"
 
-TEST_CASE("Testing set vector values")
-{
-    Vec2 sut(0, 0); 
+// TEST_CASE("Testing set vector values")
+// {
+//     Vec2 sut(0, 0); 
 
-    sut.set(5, 5);
-    REQUIRE(sut.x == 5.0f);
-    REQUIRE(sut.y == 5.0f);
-}
+//     sut.set(5, 5);
+//     REQUIRE(sut.x == 5.0f);
+//     REQUIRE(sut.y == 5.0f);
+// }
 
 TEST_CASE("Another test case")
 {
     REQUIRE(1 == 1);
-    REQUIRE(0 == 0);
 }
-
-// Acho que seria legal começar por essa classe pra criar os testes, 
-// e realmente tentar seguir as coisas do livro, pra ver se aprendo 
-// fazendo. 
